489d0547ad96ab0e1e055f242cbc023095124f88
Allzer <109965453+Allzer@users.noreply.github.com>
2024-02-08 05:36:37 +0400

    Добавил Celery and Flower

--- main.py
+++ main.py
@@ -13,32 +13,38 @@ from src.auth.models import User
 from src.auth.schemas import UserRead, UserCreate
 
 from src.operations.router import router as router_operation
+from src.tasks.router import router as router_tasks
 
+
+#Создаём приложение
 app = FastAPI(
     title="Trading App"
 )
 
+#Роутеры регистрации и авторизации
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
 )
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["auth"],
 )
-
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
 )
 
+
+#Роутер операций
 app.include_router(router_operation)
-
-
 current_user = fastapi_users.current_user()
+
+#Роутер tasks
+app.include_router(router_tasks)
+
 
 @app.get("/protected-route")
 def protected_route(user: User = Depends(current_user)):
@@ -48,7 +54,6 @@ def protected_route(user: User = Depends(current_user)):
 @app.get("/unprotected-route")
 def unprotected_route():
     return f"Hello, anonym"
-
 
 @app.on_event("startup") #Функция startup выполняется при запуске приложения
 async def startup():
---
+++ src/tasks/router.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, BackgroundTasks, Depends
+
+from src.auth.base_config import current_user
+
+from .tasks import send_email_report_dashboard
+
+router = APIRouter(prefix='/report')
+
+@router.get('/dashboard')
+def get_dashboard_report(user=Depends(current_user)):
+    send_email_report_dashboard.delay(user.username)
+    return {
+        'status': 200,
+        'data': "Письмо отправлено",
+        'details': None
+    }
+
+#Как сделать фоновую задачу на FastAPI
+#@router.get('/dashboard')
+#def get_dashboard_report_fastapi(background_tasks: BackgroundTasks, user=Depends(current_user)):
+#    background_tasks.add_task(send_email_report_dashboard, user.username) #Передаём функцию, которую нужно вызвать. Не вызываем, но передаём аргументы
+#    return {
+#        'status': 200,
+#        'data': "Письмо отправлено",
+#        'details': None
+#    }
+
+
+
+
+
